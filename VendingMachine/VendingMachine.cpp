e75752d9cabe8750d2b6ef7c0caffde8c42606ab
Trijeux <anthonybarman2003@gmail.com>
2023-10-18 12:40:34 +0200

    Upgarde VendingMachine.cpp

--- VendingMachine/VendingMachine.cpp
+++ VendingMachine/VendingMachine.cpp
@@ -4,20 +4,36 @@
 
 int main()
 {
-	Machine machineCantine("Cantine",25, 100);
+	bool Test1 = 0;
+	bool Test2 = 1;
+	do
+	{
+		do
+		{
+			Machine machineCantine("Cantine",25, 100);
 
-	machineCantine.Display();
+			machineCantine.Display();
 
-	machineCantine.ReffilleCakes(5, "Choco");
-	machineCantine.ReffilleCakes(5, "Vanille");
+			machineCantine.ReffilleCakes(5, "Choco");
+			machineCantine.ReffilleCakes(5, "Vanille");
 
-	machineCantine.Display();
+			machineCantine.Display();
 
-	machineCantine.TakeCakes(2, "Choco");
-	machineCantine.TakeCakes(1, "Vanille");
+			machineCantine.TakeCakes(2, "Choco");
+			machineCantine.TakeCakes(1, "Vanille");
 	
 
-	machineCantine.Display();
-
+			machineCantine.Display();
+		}
+		while (Test1);
+		char Test;
+		std::cout << "Veux tu recomencer" << std::endl;
+		std::cin >> Test;
+		if (Test == 'n')
+		{
+			Test2 = 0;
+		}
+	}
+	while (Test2);
 	std::cin.ignore();
 }
